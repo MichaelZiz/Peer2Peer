706a65297d05ae08a34f9ccbc2b1b399c9afabd6
MichaelZiz <33437087+MichaelZiz@users.noreply.github.com>
2019-06-06 16:57:52 -0400

    Add files via upload

---
+++ p2p2/1/clientserver-11-05.c
@@ -0,0 +1,432 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <fcntl.h> 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+
+#define BUFLEN 256	/* buffer length */
+#define dataSize 100
+#define SERVER_TCP_PORT 3000	/* well-known port */
+
+//----------------------------------
+
+  int echod(int);
+  void reaper(int);
+  
+
+  //-------------------------------------
+struct dataudp
+	{
+		char type;
+		char data[dataSize];		
+		
+		
+	};
+
+
+struct dataudp udp(struct dataudp txu, int flag) // should probably name it like px then make tx=px, should only be passed in after a DOWNLOAD stay woke 
+// accept a struct tx and rx?? i think main only sends a type not data,
+// so clear data in main, but we return a full struct, make sure it is NULL so strcat works below
+{
+	 char peername[20],contentname[20],ipnumber[20],portnum[20]="10001";
+	char c;
+	int port;
+	
+	port=atoi(portnum);
+	//set up portnum, this value cannot change and remember to close sockets
+	
+	
+  struct dataudp rxu;  //check this txu
+	//----------THIS PART CREATES THE UDP SOCKET----------------\\
+  
+ int udpsock, nBytes,i,n;
+  struct sockaddr_in serverAddr;
+  socklen_t addr_size;
+  
+  udpsock = socket(AF_INET, SOCK_DGRAM, 0);
+  memset(&serverAddr, '\0', sizeof (serverAddr)); 
+
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(port);
+  serverAddr.sin_addr.s_addr = INADDR_ANY;
+  //------------------------------------------------------------\\
+  
+ 
+
+  
+  while(1){
+	  //if download complete TCP should send rxu.type of R-register
+	  if(txu.type!='R'){ // there is no char assigned to type to we take input, this rxu is from MAIN
+	  printf("R-register content, S-Search for Content to download, T-De-reg content, L-LIST, Q-DELETE ALL\n"); //got to add in Q
+	  scanf("%c",&txu.type); // shouldbe char
+	  }
+	  
+	  if(txu.type=='R'){
+		
+			printf("Connection to index server. Enter Peer Name:\n");
+			scanf("%s",peername);
+			//if(txu.data[0]=='\0'){ //if its empty then it means that a download hasn't occurred 
+			///////
+					strcpy(txu.data, peername);
+					printf("Enter Content Name:\n");
+					scanf("%s",contentname);
+					strcat(txu.data," ");
+					strcat(txu.data,contentname);
+					///////  
+					printf("enter IP #\n"); // what is the address if we are going to be running two locals??
+					scanf("%s",ipnumber);
+					strcat(txu.data," ");
+					strcat(txu.data,ipnumber);
+					///////
+					
+					strcat(txu.data," ");
+					//printf(txu.data);
+					strcat(txu.data,portnum);
+					//printf(txu.data);
+			//}
+			///////
+			//printf(txu.data);
+			sendto(udpsock,&txu, dataSize+ 1,0,(struct sockaddr *)&serverAddr,sizeof(serverAddr)); //check for n, size
+			recvfrom(udpsock, &rxu, dataSize + 1, 0, (struct sockaddr *)&serverAddr,&addr_size);
+			
+			if(rxu.type=='E'){			
+					//print rx.data to show error message 
+					printf(rxu.data);
+					printf("\n returning to main menu\n");
+			
+					}
+		
+			if(rxu.type=='A'){ 
+					printf("%c\n", rxu.type);
+					printf("\nContent and PeerName registered, returning to main menu\n");
+				//////// NEED TO MEMSET SOMEWHERE
+				}
+		 
+		  txu.type='E';
+			if(flag==1){
+				printf("in flag\n");
+				break;
+				}
+	  }
+	  if(txu.type=='S'){
+		  
+			printf("Search: Enter Peer Name:\n");
+			scanf("%s",peername);
+			strcpy(txu.data, peername);
+			///////
+			printf("Search: Enter Content Name:\n");
+			scanf("%s",contentname);
+			strcat(txu.data," ");
+			strcat(txu.data,contentname);
+			//////// NEED TO MEMSET SOMEWHERE, did it at end of while loop
+			
+			sendto(udpsock,&txu, dataSize+ 1,0,(struct sockaddr *)&serverAddr,sizeof(serverAddr)); //check for n, size
+			recvfrom(udpsock, &rxu, dataSize + 1, 0, (struct sockaddr *)&serverAddr,&addr_size);
+			
+			if(rxu.type=='E'){			
+					//print rx.data to show error message 
+					printf(rxu.data);
+					printf("\n returning to main menu \n");
+			
+					}
+		
+			if(rxu.type=='S'){ // rxu is supposed to return to the main fxn should be able to have return rx; at end of while
+					/*here rxu should change to rxu.type=D for the TCP header*/
+					printf("%c\n", rxu.type);//rxu supposed to hold, content name, address and port, this gets passed
+					printf("\n MATCH IN INDEX!\n");
+					printf(rxu.data);
+					printf("\n");
+					rxu.type= 'D'; 
+					/***memset(&rxu.data[0], 0, dataSize);**/ //not to sure about this memset, cause we wanna keep the data we pass to main
+					break; // should break from here to enter TCP cxn, should have a return r; 
+				//////// NEED TO MEMSET SOMEWHERE
+				}
+			
+		
+		  
+	  }
+	  if(txu.type=='T'){//dereg specific content belonging to this server from index 
+	  
+			printf("Enter Content Name:\n");
+			scanf("%s",contentname);
+			strcpy(txu.data, contentname);
+
+			//strcat(txu.data,contentname);
+			strcat(txu.data," ");
+			//printf("enter Port # to delete all files from index\n");
+			//scanf("%s",portnum);
+			strcat(txu.data,portnum);
+			//strcat(txu.data,'\n');
+		    sendto(udpsock,&txu, dataSize+ 1,0,(struct sockaddr *)&serverAddr,sizeof(serverAddr)); //check for n, size
+			// here the index server is to check for the port number which is the last string after a null or space
+			//any text document matching that port num inside will be removed from the index server and deleted
+			
+			
+			recvfrom(udpsock, &rxu, dataSize + 1, 0, (struct sockaddr *)&serverAddr,&addr_size);
+			// index server should send an Ack to make sure it we deleted the file 
+			
+			if(rxu.type=='E'){			
+					//print rx.data to show error message 
+					
+					printf(rxu.data);
+					printf("\n");
+			
+					}
+		
+			if(rxu.type=='A'){ 
+					printf(rxu.data);
+					printf("\n");
+					printf("De-registered");
+					printf("\n");
+				//////// NEED TO MEMSET SOMEWHERE
+				}
+		  
+	  }
+	  if(txu.type=='L'){
+			int n=0;
+		    	
+			sendto(udpsock,&txu, dataSize+1,0,(struct sockaddr *)&serverAddr,sizeof(serverAddr));
+			printf("available content");
+	while ((n = recvfrom(udpsock, &rxu, dataSize + 1, 0, (struct sockaddr *)&serverAddr,&addr_size)) >0){ // gotta CHECK THIS WHILE LOOP
+		
+			
+			printf(rxu.data);// this isnt the same rxu as the input, this displays what list there is
+			printf("\n"); // in the index server insert \n for new contents available
+		
+		printf("%d\n",n);
+		memset(&rxu.data[0], '\0', dataSize); // check this out, this is needed to continuously read the data
+				}
+		
+			
+		  
+		  
+	  }
+	  if(txu.type=='Q'){// dereg ALL content belonging to this server
+		  //delete everything in index
+			
+			//printf("Enter Content Name:\n");
+			//scanf("%s",contentname);
+			
+			strcpy(txu.data, "content");
+			strcat(txu.data," ");
+			//printf("enter Port # to delete all files from index\n");
+			//scanf("%s",portnum); 
+
+			strcat(txu.data,portnum);
+			
+		  sendto(udpsock,&txu, dataSize+1,0,(struct sockaddr *)&serverAddr,sizeof(serverAddr));
+		  recvfrom(udpsock, &rxu, dataSize + 1, 0, (struct sockaddr *)&serverAddr,&addr_size);
+				if(rxu.type=='E'){			
+					//print rx.data to show error message 
+					
+					printf(rxu.data);
+					printf("\n");
+			
+					}
+		
+				if(rxu.type=='A'){ 
+					printf(rxu.data);
+					printf("\n");
+					printf("De-registered");
+					printf("\n");
+				//////// NEED TO MEMSET SOMEWHERE
+				}
+		
+		    
+		  
+	  }
+	  
+	  memset(&rxu.data[0], '\0', dataSize); // reset the data size every time u restart this function in the while loop
+	  //outside the while loop i dont think it matter because u will exit function and data lost 
+  }
+  close(udpsock);
+	if(flag==1){
+	rxu.type=' ';
+	return rxu;
+		}
+  // should be a return here for the S pdu, we pass the address and contents back to TCP cxn
+  return rxu;
+	  
+  }
+	
+	
+	
+
+
+//PEER 1
+int main(int argc, char **argv){
+	
+//--------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------
+	  //this part should create the TCP SOCKET
+			
+	int 	sd, new_sd, client_len, port;
+	struct	sockaddr_in server, client;
+
+	switch(argc){
+	case 1:
+		port = SERVER_TCP_PORT;
+		break;
+	case 2:
+		port = atoi(argv[1]);
+		break;
+	default:
+		fprintf(stderr, "Usage: %d [port]\n", argv[0]);
+		exit(1);
+	}
+
+	/* Create a stream socket	*/	
+	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		fprintf(stderr, "Can't creat a socket\n");
+		exit(1);
+	}
+
+	/* Bind an address to the socket	*/
+	bzero((char *)&server, sizeof(struct sockaddr_in));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (bind(sd, (struct sockaddr *)&server, sizeof(server)) == -1){
+		fprintf(stderr, "Can't bind name to socket\n");
+		exit(1);
+	}
+
+	/* queue up to 5 connect requests  */
+	listen(sd, 5);
+
+	(void) signal(SIGCHLD, reaper);
+
+	while(1) {
+	  client_len = sizeof(client);
+	  new_sd = accept(sd, (struct sockaddr *)&client, &client_len);
+	  if(new_sd < 0){
+	    fprintf(stderr, "Can't accept client \n");
+	    exit(1);
+	  }
+	  switch (fork()){
+	  case 0:		/* child */
+		(void) close(sd);
+		exit(echod(new_sd));
+	  default:		/* parent */
+		(void) close(new_sd);
+		break;
+	  case -1:
+		fprintf(stderr, "fork: error\n");
+	  }
+	}
+}
+//--------------------------------------------------------------------------------	
+
+//--------------------------------------------------------------------------------
+	
+	
+int echod(int sd)
+{	int flag=0;
+	struct dataudp tx, rx;
+	char c;
+	
+	printf("TCP Connection Established\n");
+	
+	while(1){
+
+	if(flag==0){
+		tx=udp(tx,flag);		// should we have argument into the udp fxn, this returns type D structure with data that includes
+			}			//contentname+ipname+portname
+						// check how this TCP CXN works
+						// how to close the UDP socket
+//--------------------------------------------------------------------------------
+
+		char contentname[20],ipname[20],portname[20],help[100];
+		sscanf(tx.data, "%s %s %s", contentname, ipname, portname); // this is the portname and ipname of the located server
+		int port= atoi(portname);
+
+		
+		memset(&tx.data[0], '\0', dataSize); // should be zero or null
+		strcpy(tx.data, contentname);
+		printf("type %c\n",tx.type);
+	if(tx.type=='D'){
+			send(sd,&tx,101,0);
+		 	//Successful connection, start transmitting C PDUs
+			int n;
+			int fp;
+			//fp=open(contentname, O_WRONLY);
+				FILE * fpp;
+				printf("%s",contentname);
+				fpp = fopen(contentname, "a");
+			while ((n = recv(sd, &rx, 101, 0)) > 0){
+				printf("here %c\n", rx.type);
+				printf("%s",rx.data);
+				fprintf(fpp,"%s",rx.data);
+				//write(fp,rx.data,100);
+					if (rx.type == 'F'){
+						printf("%d",n);
+						memset(&tx.data[0], '\0', dataSize);
+						printf("File transfer complete!\n");
+						break;
+					}
+				//memset(&tx.data[0], '\0', dataSize);
+				//Reconnect to index server here, send TX.type R type and TX.data content/ip/port
+				//here you are writing to the file and clear mem everytime so u dont have overwriting data
+			}
+			//setting up parameters for the register
+			tx.type='R';
+			/**/// put down the portnum and ipname for this file(peer) ip name should be the same, same with the port
+			strcpy(contentname, tx.data);
+			strcat(tx.data, " ");
+			strcat(tx.data, ipname);
+			strcat(tx.data, " ");
+			strcat(tx.data,"10001");
+			// tx.data= contentname+ " "+ ipname+ " "+10011
+			flag=1;
+			tx=udp(tx,flag);
+			
+	}else { //Downloading
+	 printf("here in transmit\n");
+	recv(sd, &rx,101, 0); // info what file to send
+	printf("%s\n",rx.data);
+	if(rx.type=='D'){
+		int i = 0;
+		/*
+		while(rx.data[i]!= " "){
+		 content[i]=rx.data[i];
+		 i++;
+			
+		}*/
+		sscanf(rx.data,"%s",contentname); // read filename
+		
+		int fp;
+		fp=open(contentname, O_RDONLY);
+		if(fp>0){ // open file and transmit
+			while((i = read(fp, tx.data, 100))>0){
+				tx.type = strlen(tx.data) * sizeof(char) < 100 ? 'F' : 'C';
+				printf("%c",tx.type);
+				send(sd,&tx,101,0);
+				memset(&tx.data[0], '\0', dataSize);
+			}
+			close(fp); // close the file
+		}else{// no such file exists
+			printf("No file exists\n");
+		}
+		flag=0;
+	}		
+  }
+}
+return 0;
+}		//REMEMBER THAT THE OTHER PEER FILE SHOULD HAVE A DIFFERENT PORTNUM TO INDEX SERVER***
+//-------------------------------------------------------------WASTELAND ----------------------------			
+		
+void	reaper(int sig)
+{
+	int	status;
+	while(wait3(&status, WNOHANG, (struct rusage *)0) >= 0);
+}
---
+++ p2p2/i/indexserverNOV5.c
@@ -0,0 +1,332 @@
+/* A simple server using UDP */
+/*#include <stdio.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h> 
+#include <stdlib.h> 
+#include <arpa/inet.h> 
+#include <fcntl.h> 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stddef.h>
+#include <string.h>*/
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <fcntl.h> 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stddef.h>
+
+
+
+#define BUFLEN 256 	/* buffer length */
+#define dataSize 100 	
+struct dataudp
+	{
+		char type;
+		char data[dataSize];		
+		
+		
+	};
+
+// remember to declare fxns at top
+struct dataudp  RandS(struct dataudp rx){ // RX should be S(download we want to find a match so we read only first while loop) or R 
+	struct dataudp sx,rrx;
+	char peername[20],contentname[20],ipname[20],portname[20];
+	char peername1[20],contentname1[20],ipname1[20],portname1[20];
+	char string[100];
+	int empty;
+			FILE* fp =fopen("IndexServer.txt","r");
+			//no memset cause we comparing fxn
+			empty = 1;
+			
+///		
+		if(fp != NULL){
+			while(!feof(fp)){
+	//this while loop is meant to read the data from the file
+				fgets(string,100,fp);
+				empty = 0;
+				sscanf(string,"%s %s %s %s",peername,contentname,ipname,portname);
+		
+				if(rx.type=='R'){
+					sscanf(rx.data,"%s %s %s %s",peername1,contentname1,ipname1,portname1);
+				}
+				if(rx.type=='S'){
+					sscanf(rx.data,"%s %s",peername1,contentname1);
+					
+				}	
+				
+				
+				if(strcmp(contentname,contentname1)==0){
+					if(strcmp(peername,peername1)==0){
+						//if content and peer name exist is an error for R type
+						//if content and peer name exist is good for S type
+						if(rx.type=='R'){
+						rrx.type='E';
+						strcpy(rrx.data, "Content/Peer Exist Choose other ");
+						break; // found a match break out of the loop error
+						}
+						if(rx.type =='S'){ // transmit the address back and port for search
+						sx.type='S';
+						strcpy(sx.data, contentname);
+						strcat(sx.data, " ");
+						strcat(sx.data, ipname);
+						strcat(sx.data, " ");
+						strcat(sx.data, portname);
+						break;	//break out of loop to continue download
+						}
+					}
+					else{ // same content diff peer
+						if(rx.type=='R'){
+							rrx.type='A';
+							strcpy(rrx.data, "\n Success, same content different peer valid, peer that has that content ");
+							strcat(rrx.data, peername);
+							strcat(rrx.data, "\n");
+						}
+						if(rx.type=='S'){
+							sx.type='E';
+							strcpy(sx.data, "\n SEARCH DNE \n");
+
+							
+						}
+					}
+					
+				}else{ //if content doesn't exist then can register
+						if(rx.type=='R'){
+							rrx.type='A';
+							strcpy(rrx.data, "\n Success, content DNE \n");
+						}
+						if(rx.type=='S'){
+							sx.type='E';
+							strcpy(sx.data, "\n SEARCH DNE \n");							
+						}
+				}
+				
+				
+				
+			}
+}
+else{printf("couldnt open file");}
+			if (empty == 1){
+				printf("File Empty\n");
+				if(rx.type=='R'){
+					rrx.type='A';
+					
+				}
+				if(rx.type=='S'){
+					sx.type='E';
+					strcpy(sx.data, "\n SEARCH DNE \n");							
+				}
+			}
+			
+			fclose(fp);
+printf("%c",rrx.type);
+			/*if((rx.type=='R' || rx.type=='S')&& (rrx.type!='S' && rrx.type!='A' && rrx.type!='E')){
+				rrx.type='E';
+				strcpy(rrx.data,"Error: empty index\n");
+
+				return rrx;
+
+				}
+*/
+			if(rx.type=='R' && rrx.type=='E'){// THIS IS FOR THE REGISTER return back Etype with ERRMSG
+				return rrx;
+				
+			}
+			if(rx.type=='S' && sx.type=='S'){//THIS is to return portnum and address immediately 
+				return sx;
+			}
+			if(rx.type=='S' && sx.type=='E'){//THIS is to return portnum and address immediately 
+				return sx;
+			}
+			if(rx.type=='R' && rrx.type=='A'){// SENDS acknowledge PDU after we reg to index file 
+				
+			FILE* fp =fopen("IndexServer.txt","a");
+			
+			fprintf(fp,"\n %s",rx.data); // check out this new line character to make sure it doesn't mess up array/file
+			
+			fclose(fp);
+			printf(rrx.data);
+			strcpy(rrx.data, "\n Success, content DNE \n");
+
+				return rrx; // return back ack pdu
+			}
+			
+			
+	
+	
+}
+
+struct dataudp QandT(struct dataudp rx){
+	char peername[20],contentname[20],ipname[20],portname[20];
+	char peername1[20],contentname1[20],ipname1[20],portname1[20];
+	char string[100];
+	struct dataudp tx;
+	char *buffer;
+	char * ptr;
+	buffer= (char *)malloc(1000*sizeof(char)); //CHECK THIS OUT size of buffer ?? 1000?
+	ptr = buffer;
+	//no memset in here because we receive the data and we just compare it, not rewriting over it
+	FILE* fp =fopen("IndexServer.txt","r"); // read and write 
+	while(fgets(string,100,fp)){	//this while loop is meant to read the data from the file
+				
+				
+				
+				/////////////
+				sscanf(string,"%s %s %s %s",peername,contentname,ipname,portname);
+
+				///////////////////////////
+				sscanf(rx.data,"%s %s",contentname1,portname1);
+			   	////////////////////////////////
+				if(rx.type== 'Q'){
+					if(strcmp(portname,portname1)==0){ // if they same port number don't copy into buffer
+					
+					}else{
+						strcpy(ptr,string);	// different ports copy into buffer	
+					
+				   }
+				}
+				
+				if(rx.type== 'T'){ // if content the same and port num the same dont copy into buffer
+				if((strcmp(contentname,contentname1)==0) && (strcmp(portname,portname1)==0)){
+					//not copying into the buffer deletes that line
+				}else{
+					strcpy(ptr,string);				
+					
+				}
+				}
+				
+				
+			}
+			
+			fclose(fp);// close the old file with all the port/content
+			
+			int ret= remove("IndexServer.txt"); // delete the file 
+			 //open new file with same name
+			fp =fopen("IndexServer.txt","w"); // now opening same file to rewrite over, with removed ports/contents
+			fprintf(fp,"%s",buffer); // write buffer to file
+			fclose(fp);
+			
+			
+			
+			if(rx.type=='Q'){
+					tx.type='A'; 
+					strcpy(tx.data, "\n Removed All \n");
+
+			}
+			if(rx.type=='T'){
+					tx.type='A'; 
+					strcpy(tx.data, "\n Removed Specific Content \n");
+
+			}
+			
+			
+			return tx;
+	
+	
+	
+}
+
+int main(){//int argc, char **argv
+	int port=0;
+	printf("Enter the udp port num:");
+	scanf("%d",&port);
+	// how to close the UDP socket
+	///////////////////////////////////////////
+	int udpsock;
+	char string[100];
+	struct sockaddr_in server, client;
+	socklen_t addr_size; 
+	
+	udpsock=socket(AF_INET,SOCK_DGRAM,0);
+	memset(&server, '\0', sizeof(server)); // declare it in the other functions too, may have to pass size of server as a parameter
+	
+	server.sin_family=AF_INET;
+	server.sin_addr.s_addr=INADDR_ANY;
+	server.sin_port=htons(port);// connect to the first peer at address 10001 //atoi(argv[1])
+	
+	bind(udpsock,(struct sockaddr*)&server, sizeof(server)); 
+	////////////////////////////////////////////////
+	struct dataudp tx, rx;
+
+	addr_size=sizeof(client);
+	
+	
+	while(1){
+		recvfrom(udpsock, &rx, dataSize+1, 0, (struct sockaddr *)&client,&addr_size);
+		//printf("sruck62");
+		//printf("%c",rx.type);
+	if(rx.type=='R'){
+			//check the file first then write to it, check if the file exists first 
+			//methods of approaching the problem: 
+			
+			//3) 1 file and each row of file contains the parameters for that one content, need to read file and read row by row
+			tx=RandS(rx);
+			sendto(udpsock,&tx, dataSize+ 1,0,(struct sockaddr *)&client,addr_size); //check for n, size
+			
+	}
+	if(rx.type=='S'){
+			//1) search and return the , an either send E or S type 
+			tx=RandS(rx);
+			sendto(udpsock,&tx, dataSize+ 1,0,(struct sockaddr *)&client,addr_size); //check for n, size
+			
+	}
+	if(rx.type=='T'){
+			//printf("sruck61");
+			//this will open all the files in the directory and check for the port number and the content, 
+			//*** then check if if equals port/address number , or if file has, 1(peer1) or 2(peer2) delete accordingly 
+			//if port num matches then it will delete the file
+			tx= QandT(rx);
+			sendto(udpsock,&tx, dataSize+ 1,0,(struct sockaddr *)&client,addr_size); //check for n, size
+			
+	}
+	if(rx.type=='L'){
+
+			
+			
+
+			//printf("sruck6");
+
+
+			// this will basically print out every file in the directory ex. ls
+			FILE* fo =fopen("IndexServer.txt","r"); // read 
+			//printf("stuck5");			
+			while(fgets(string,100,fo)){
+
+					//if(string[100]==EOF){break;}
+				//	printf("stuck4");
+				strcpy(tx.data,string);
+				//printf("stuck1");
+				sendto(udpsock,&tx, dataSize+ 1,0,(struct sockaddr *)&client,addr_size); //check for n, size
+				//printf("stuck2");
+				memset(&tx.data[0], 0, dataSize); // NULL values?
+				
+				//printf("stuck3");		
+			}
+			//printf("unstuck");
+			fclose(fo); 
+
+			
+	}
+	if(rx.type=='Q'){
+			// just delete every file in directory
+			tx= QandT(rx);
+			sendto(udpsock,&tx, dataSize+ 1,0,(struct sockaddr *)&client,addr_size); //check for n, size
+			
+	}
+	
+	memset(&rx.data[0], '\0', dataSize); //unclear if this should be NULL or 0
+	}
+	return 0;
+	
+}
